41612c48eacdf1f64516198c474b35796e7ceeb4
zzzsyyy <59917878+zzzsyyy@users.noreply.github.com>
2020-11-05 20:39:51 +0800

    Update 解一元二次方程.py

--- Others/解一元二次方程.py
+++ Others/解一元二次方程.py
@@ -1,14 +1,14 @@
 #求解一元二次方程
 
 print('解ax²+bx+c=0')
-zm = 'abc'
+co = 'abc'
 ls = [0,0,0]
 for i in range(3):
-    z = str(zm[i])
+    z = str(co[i])
     ls[i] = eval(input("请输入{0:}的值:".format(z)))
 a,b,c = ls[0],ls[1],ls[2]
 det = pow(b,2)-4*a*c
-def qiujie():
+def solve():
     if det<0:
         print('方程无实数根')
     else:
@@ -26,5 +26,5 @@ def main():
         else:
             print('x={0:}'.format(-c/b))
     else:
-        qiujie()
+        solve()
 main()
