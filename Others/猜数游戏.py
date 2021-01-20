691f8444dae3ae7562d73ce2e4dc65af909f0acc
zzzsy <zsy3141592@163.com>
2021-01-20 09:12:50 +0800

    更新

--- Others/猜数游戏.py
+++ Others/猜数游戏.py
@@ -1,10 +1,6 @@
 from random import *
 
-
-
 is_error = 1
-
-#answer = ['5','1','1','1']
 
 #将得到的输入拆分并装入列表
 def cut(a):
@@ -12,27 +8,26 @@ def cut(a):
     ls.extend(a)
     return ls
 
-    
 #比较两个数，返回答案中有你的几个数
 #m = answer   n = your_answer
 def compare_1(m,n):
-    counter_1 = 0
+    count_1 = 0
     set_answer = set(m)
     ls_temp = []
     for i in n:
         if i in set_answer and i not in ls_temp:
-            counter_1 += 1
+            count_1 += 1
             ls_temp.append(i)
-    return counter_1
+    return count_1
 
 #比较两个数，返回位置正确的个数
 def compare_2(m,n):
     
-    counter_2 = 0
+    count_2 = 0
     for i in range(4):
         if m[i] == n[i]:
-            counter_2 += 1
-    return counter_2
+            count_2 += 1
+    return count_2
 
 #处理输入不规范的情况
 def iserror(a):
@@ -44,31 +39,27 @@ def iserror(a):
     return a.isdigit()==1 and len(a)==4#正常返回 1 ，否则返回 0
 
 
-
-    
-
-
 #主函数        
 def main():
-    counter_2 = counter_1 = counter = 0
+    count_2 = count_1 = count = 0
     answer = list(str(randint(1000,9999)))
-    while counter_2 !=  4:
+    while count_2 !=  4:
         your_input = input("请键入你的猜想：")
         is_error = iserror(your_input)
         while is_error == 0:
             break
         else:
             your_answer = cut(your_input)
-            counter += 1
-            counter_1 = compare_1(answer,your_answer)
-            counter_2 = compare_2(answer,your_answer)
-            if counter_2 != 4:
-                print("你说中了{}个数字，有{}个数字位置正确！".format(counter_1,counter_2))
+            count += 1
+            count_1 = compare_1(answer,your_answer)
+            count_2 = compare_2(answer,your_answer)
+            if count_2 != 4:
+                print("你说中了{}个数字，有{}个数字位置正确！".format(count_1,count_2))
             else:
                 print("终于猜对了！！！")
-                print("你用了{}次！".format(counter))
-                
-                
+                print("你用了{}次！".format(count))
+
+
 def naother():
     main()
     tem = input("按y重新游戏，按Enter退出")
